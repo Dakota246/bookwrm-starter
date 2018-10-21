e83a0718ae189de7bfadc936ba00d038c4a8a400
dakota246 <dmnatour@cps.edu>
2018-10-21 14:10:39 -0500

    books reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reviews, dependent: :destroy
+
+  def full_name
+    return 'Anonymous' unless (first_name && last_name)
+    first_name + " " + last_name
+  end
+
+  def initials
+    return 'AN' unless (first_name && last_name)
+    first_name.try(:first) + " " + last_name.try(:first)
+  end
 end
